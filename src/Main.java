5231e76e1635ff125ada60cd37ea611485bc5afb
”Messmer” <2429611912@qq.com>
2025-02-21 13:49:36 +0800

    my first commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,21 @@
 public class Main{
-    public static void main(String[] args) {
-        
-        MusicBox mbox = new MusicBox("S34TG65", 'Y', "Raindrops", "Misty", 
-        "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
+        public static void main(String[] args) {
 
+        MusicBox mbox = new MusicBox("S34TG65", 'Y', "Raindrops", "Misty", 
+         "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
+    
         System.out.println("Now playing the song");
         mbox.playSong(mbox.getSongID(), mbox.getPremiumSong(), 2);
-        
-        // --- TODO
 
-        /* Repeat the above code to create another object  
-         * Pass different parameters to the MusicBox constructor, but initialise premiumSong to N and when calling the
-         * playSong method, pass ads parameter as just 1
-         * 
-         * 
-        */
-        
-
-    }
+        MusicBox anotherMbox = new MusicBox("S78XY90", 'N', "Sunshine Days", "Sunny",
+        "Happy Vibes", "Pop", "Star Studio", "Melody Magic");
+        System.out.println("Now playing another song");
+        anotherMbox.playSong(anotherMbox.getSongID(), anotherMbox.getPremiumSong(), 1);
+    
+ }
 }
-
-
-
-
+    
+    
+    
+    
+    
